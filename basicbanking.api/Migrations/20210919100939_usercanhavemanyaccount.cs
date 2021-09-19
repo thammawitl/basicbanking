a4b7746aeb8d9de1c760c28e79ffce701d3d6767
Thammawit Lerdthanavaranont <legativity@gmail.com>
2021-09-19 18:41:55 +0700

    [+] update one to many user to bankaccount

---
+++ basicbanking.api/Migrations/20210919100939_usercanhavemanyaccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace basicbanking.api.Migrations
+{
+    public partial class usercanhavemanyaccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BankAccounts_UserId",
+                table: "BankAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_UserId",
+                table: "BankAccounts",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BankAccounts_UserId",
+                table: "BankAccounts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Users",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_UserId",
+                table: "BankAccounts",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
